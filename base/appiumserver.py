56b6a91bc8a7c48cf0f04f90cff1c72a598f54ea
Unmurphy <yzx520yzx@126.com>
2018-02-24 15:49:43 +0800

    Update appiumserver.py

--- base/appiumserver.py
+++ base/appiumserver.py
@@ -13,7 +13,7 @@ class AppiumServer:
 
     def start_server(self):
         self.stop_server()
-        cmd = "/usr/local/bin/node /usr/local/lib/node_modules/appium/build/lib/main.js --session-override -a %s -p %s" % (self.host, self.port)
+        cmd = "appium --session-override -a %s -p %s" % (self.host, self.port)
         appium_process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1,
                                           close_fds=True)
         is_start = False
@@ -21,7 +21,6 @@ class AppiumServer:
         while not is_start and (time.time() - start_time) < float(self.timeout):
             appium_line = appium_process.stdout.readline().strip().decode()
             time.sleep(1)
-            print(appium_line)
             print("---------start_server----------")
             if 'listener started' in appium_line:
                 print("---------start_server_success----------")
