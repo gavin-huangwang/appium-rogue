c1b3e3e0c60091b89bc3f9f668feda6dcd3bf4d1
Unmurphy <yzx520yzx@126.com>
2018-02-24 14:04:35 +0800

    Update appiumserver.py

--- base/appiumserver.py
+++ base/appiumserver.py
@@ -13,7 +13,7 @@ class AppiumServer:
 
     def start_server(self):
         self.stop_server()
-        cmd = "/usr/local/bin/appium --session-override -a %s -p %s" % (self.host, self.port)
+        cmd = "/usr/local/bin/node /usr/local/lib/node_modules/appium/build/lib/main.js --session-override -a %s -p %s" % (self.host, self.port)
         appium_process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1,
                                           close_fds=True)
         is_start = False
